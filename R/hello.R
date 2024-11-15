730cee29b65334ee931a613393e42ebd44e703e0
Sebastian Krause <sebastian.krause94@t-online.de>
2024-11-15 17:05:42 +0100

    updated readme.

--- R/hello.R
+++ R/hello.R
@@ -1,5 +1,5 @@
 library(shiny)
-library(ConfidenceIntervals)
+#library(ConfidenceIntervals)
 library(bslib)
 
 ui <- page_fluid(
@@ -17,7 +17,7 @@ server <- function(input, output) {
 
     # The widget
     ConfidenceIntervals(insertion_type = "OnlyIntervals",
-                        axis_limits = c(1,9),show_legend= FALSE, show_tooltip = FALSE, background_color = "#E11313",
+                        axis_limits = c(1,9),show_legend= FALSE, show_tooltip = FALSE, background_color = "#0466f9",
                         color_main = "#E11313", chart_data = c(5,3,5,6), top_band= c(6,6,6,6), bottom_band = c(4,4,4,4), labels= c("Eins", "zwei", "drei", "vier", "sechs"))
   })
 
